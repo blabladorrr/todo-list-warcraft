fdad84adfbe0e4744935b953cf7a16fe57d665c8
neonvivec <blabladorrr@gmail.com>
2023-07-03 21:53:21 +0200

    fix: Add new task empty content check

--- src/App.js
+++ src/App.js
@@ -40,16 +40,18 @@ function App() {
   }
 
   const addNewTask = (content) => {
-    setTasks(
-      [
-        ...tasks,
-        {
-          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-          content,
-          done: false,
-        },
-      ]
-    )
+    if (content !== "") {
+      setTasks(
+        [
+          ...tasks,
+          {
+            id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+            content,
+            done: false,
+          },
+        ]
+      )
+    }
   }
 
   return (
