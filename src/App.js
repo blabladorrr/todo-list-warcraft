9d5d89b5e0f7a77988d118819da490c3fa474e59
neonvivec <blabladorrr@gmail.com>
2023-07-03 21:47:37 +0200

    feat: Implement adding new task function and pass it to Form component

--- src/App.js
+++ src/App.js
@@ -39,13 +39,26 @@ function App() {
       )));
   }
 
+  const addNewTask = (content) => {
+    setTasks(
+      [
+        ...tasks,
+        {
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+          content,
+          done: false,
+        },
+      ]
+    )
+  }
+
   return (
     <Container>
       <Header title="ToDo list" />
       <Section
         title="Add new task"
         body={
-          <Form />
+          <Form addNewTask={addNewTask} />
         } 
       />
       <Section 
